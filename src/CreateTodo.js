89db927e90e41b7ab07ab5317630923dc281dd28
stephanieeurice <stephanieeurice1800@gmail.com>
2021-09-10 02:04:13 +0800

    trying to commit using git gui

---
+++ src/CreateTodo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useHistory } from 'react-router';
+import { createTodo } from './api';
+import { TodoForm } from "./TodoForm";
+
+export const CreateTodo = () => {
+const history = useHistory()
+
+    const onSubmit = async (data) => {
+       await createTodo(data)
+       history.push("/")
+    };
+
+    return (
+    <div className="container">
+       <div className="mt-3">
+           <h3>Create Todo Item</h3>
+            <TodoForm onSubmit={onSubmit}/>
+        </div>
+    </div>
+    );
+}
